d2eaeb730aef0ee9880935cb27612761bc49899f
jordanhagan <jordan.hagan@gmail.com>
2020-11-09 21:18:06 -0500

    Add 2019 matches to the reference data

--- back_end/family_constants.py
+++ back_end/family_constants.py
@@ -2,33 +2,41 @@ family_dict = {
     'Jan Fallon':
         {'SO': 'Chuck Fallon',
          'Likes': ['Birds', 'Reading', 'Candles'],
-         2018: 'Gabe Fallon'},
+         2018: 'Gabe Fallon',
+         2019: 'Jordan Fallon'},
     'Chuck Fallon':
         {'SO': 'Jan Fallon',
          'Likes': ['Birds', 'Running', 'Socks'],
-         2018: 'Jordan Fallon'},
+         2018: 'Katie Fornwalt',
+         2019: 'Jen Fallon'},
     'Jordan Fallon':
         {'SO': 'Jordan Hagan',
          'Likes': ['Drums', 'Video Games', 'Reading'],
-         2018: 'Jen Fallon'},
+         2018: 'Jen Fallon',
+         2019: 'Gabe Fallon'},
     'Gabe Fallon':
         {'SO': 'Jen Fallon',
         'Likes': ['Running', 'Music', 'Video Games'],
-        2018: 'Katie Fornwalt'},
+        2018: 'Chuck Fallon',
+        2019: 'Katie Fornwalt'},
     'Micah Fallon':
         {'SO': 'Katie Fornwalt',
          'Likes': ['Video Games', 'Weed', 'Heartfelt Presents'],
-         2018: 'Chuck Fallon'},
+         2018: 'Jordan Fallon',
+         2019: 'Chuck Fallon'},
     'Jordan Hagan':
         {'SO': 'Jordan Fallon',
          'Likes': ['Music', 'Flowers', 'Running'],
-         2018: 'Micah Fallon'},
+         2018: 'Micah Fallon',
+         2019: 'Jan Fallon'},
     'Jen Fallon':
         {'SO': 'Gabe Fallon',
          'Likes': ['Baking', 'Running', 'Sour Beers'],
-         2018: 'Jan Fallon'},
+         2018: 'Jordan Hagan',
+         2019: 'Micah Fallon'},
     'Katie Fornwalt':
         {'SO': 'Micah Fallon',
          'Likes': ['Holistic Body Products', 'Colorado', 'Candles'],
-         2018: 'Jordan Hagan'}
+         2018: 'Jan Fallon',
+         2019: 'Jordan Hagan'}
 }
