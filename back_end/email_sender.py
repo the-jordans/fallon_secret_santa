22b9cd53674bf201e69244c79d413c04e93fccd7
jordanhagan <jordan.hagan@gmail.com>
2020-11-29 13:05:53 -0500

    MVP Dynamic Email Generation

--- back_end/email_sender.py
+++ back_end/email_sender.py
@@ -1,13 +1,17 @@
 import os
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Mail, TemplateId, Substitution
 from secret_santa_hub import return_matches_for_everyone
 
 message = Mail(
     from_email='secret.santa@jordanfallon.com',
-    to_emails='jordan.hagan@gmail.com',
-    subject='Test Email',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    to_emails='jordan.hagan@gmail.com'
+    )
+
+message.template_id = TemplateId('d-b1b2395dc84146e4b7cd441de7f01dd2')
+message.dynamic_template_data = {
+        'giftReceiver': 'Jordan-Test',
+    }
 
 try:
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
