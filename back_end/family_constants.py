1ab7e0bd6fd2214f544f3180a01baa3a2979290c
jordanhagan <jordan.hagan@gmail.com>
2020-11-29 12:24:21 -0500

    Update with email addresses

--- back_end/family_constants.py
+++ back_end/family_constants.py
@@ -1,41 +1,49 @@
 family_dict = {
     'Jan Fallon':
         {'SO': 'Chuck Fallon',
+         'Email': 'fallon.jan@gmail.com',
          'Likes': ['Birds', 'Reading', 'Candles'],
          2018: 'Gabe Fallon',
          2019: 'Jordan Fallon'},
     'Chuck Fallon':
         {'SO': 'Jan Fallon',
+         'Email': 'chuck@chuckfallon.com',
          'Likes': ['Birds', 'Running', 'Socks'],
          2018: 'Katie Fornwalt',
          2019: 'Jen Fallon'},
     'Jordan Fallon':
         {'SO': 'Jordan Hagan',
+         'Email': 'thefetts@gmail.com',
          'Likes': ['Drums', 'Video Games', 'Reading'],
          2018: 'Jen Fallon',
          2019: 'Gabe Fallon'},
     'Gabe Fallon':
         {'SO': 'Jen Fallon',
-        'Likes': ['Running', 'Music', 'Video Games'],
-        2018: 'Chuck Fallon',
-        2019: 'Katie Fornwalt'},
+         'Email': 'gabefallo@gmail.com',
+         'Likes': ['Running', 'Music', 'Video Games'],
+         2018: 'Chuck Fallon',
+         2019: 'Katie Fornwalt'},
     'Micah Fallon':
         {'SO': 'Katie Fornwalt',
+         'Email': 'reban4@gmail.com',
          'Likes': ['Video Games', 'Weed', 'Heartfelt Presents'],
          2018: 'Jordan Fallon',
          2019: 'Chuck Fallon'},
     'Jordan Hagan':
         {'SO': 'Jordan Fallon',
+         'Email': 'Jordan.Hagan@gmail.com',
          'Likes': ['Music', 'Flowers', 'Running'],
          2018: 'Micah Fallon',
          2019: 'Jan Fallon'},
     'Jen Fallon':
         {'SO': 'Gabe Fallon',
+         'Email': 'jascott@colorado.edu',
          'Likes': ['Baking', 'Running', 'Sour Beers'],
          2018: 'Jordan Hagan',
          2019: 'Micah Fallon'},
     'Katie Fornwalt':
         {'SO': 'Micah Fallon',
+         "Email": 'kfornwalt2@gmail.com',
          'Likes': ['Holistic Body Products', 'Colorado', 'Candles'],
          2018: 'Jan Fallon',
          2019: 'Jordan Hagan'}
