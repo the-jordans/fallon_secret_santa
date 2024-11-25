076ef163c3d2d420c0772bdfbdbcb2e18654d9d4
Jordan Fallon <jordanfallon@Lil-Smoky.local>
2024-11-25 00:23:11 -0500

    Refactor note writing

--- simple/save.ts
+++ simple/save.ts
@@ -7,20 +7,18 @@ export const saveToFiles = (results: Array<SimpleResult>) => {
     const fallonSanta = theirSanta('Jordan Fallon', results)
     const haganSanta = theirSanta('Jordan Hagan', results)
     const theRest = results.filter(result => result.santa != fallonSanta.santa && result.santa != haganSanta.santa)
+    const midpoint = Math.ceil(theRest.length/2)
 
-    const fallonNotes = [haganSanta]
-        .concat(theRest.slice(0, 3))
-        .map(result => `${result.santa} is getting a gift for ${result.receiver}`)
-        .join('\n')
+    const fallonNotes = makeNotes([haganSanta].concat(theRest.slice(0, midpoint)))
+    const haganNotes = makeNotes([fallonSanta].concat(theRest.slice(midpoint)))
 
-    const haganNotes = [fallonSanta]
-        .concat(theRest.slice(3))
-        .map(result => `${result.santa} is getting a gift for ${result.receiver}`)
-        .join('\n')
-
-    writeFileSync("hagan.txt", haganNotes)
     writeFileSync("fallon.txt", fallonNotes)
+    writeFileSync("hagan.txt", haganNotes)
 }
 
 const theirSanta = (name: Name, results: Array<SimpleResult>) =>
     results.filter(result => result.receiver == name)[0]
+
+const makeNotes = (list: Array<SimpleResult>) =>
+    list.map(result => `${result.santa} is getting a gift for ${result.receiver}`)
+        .join('\n')
